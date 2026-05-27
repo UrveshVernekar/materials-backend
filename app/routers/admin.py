e8292286d96eed7ba24ed0900d9c0d4c0a337d5e
UrveshVernekar <urveshvernekar8@gmail.com>
2026-05-27 12:46:39 +0530

    PO recommendation fix

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1586,12 +1586,12 @@ def process_prediction_task(task_id: str):
                 m1_po = max(0.0, lead_time_qty - gpc_stk + m1_pred)
                 m1_mes = max(0.0, gpc_stk - m1_pred + m1_po)
 
-                # m2_po = max(0.0, m1_po + m2_pred)
-                m2_po = max(0.0, m2_pred - (safety_stock_95 / 2))
+                m2_po_suggested = max(0.0, m2_pred - (safety_stock_95 / 2))
+                m2_po = max(m2_po_suggested, lead_time_qty - m1_mes + m2_pred)
                 m2_mes = max(0.0, m1_mes + m2_po - m2_pred)
 
-                # m3_po = max(0.0, m2_po + m3_pred)
-                m3_po = max(0.0, m3_pred - (safety_stock_95 * 0.63))
+                m3_po_suggested = max(0.0, m3_pred - (safety_stock_95 * 0.63))
+                m3_po = max(m3_po_suggested, lead_time_qty - m2_mes + m3_pred)
                 m3_mes = max(0.0, m2_mes + m3_po - m3_pred)
 
                 # Calculate days equivalent using mean daily demand
